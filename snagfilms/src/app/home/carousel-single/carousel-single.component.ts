fea4f04f74d350ae16de3370df7862c3bc21e9be
gao <jackiegao2018@hotmail.com>
2018-03-23 00:09:53 -0400

    make it better

--- snagfilms/src/app/home/carousel-single/carousel-single.component.ts
+++ snagfilms/src/app/home/carousel-single/carousel-single.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input, ElementRef, Renderer2,HostBinding } from '@an
   styleUrls: ['./carousel-single.component.css']
 })
 export class CarouselSingleComponent implements OnInit {
+  show: boolean;
   @Input('item') item: any;
   @Input() height;
   @Input() font;
